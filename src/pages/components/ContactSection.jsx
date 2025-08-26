58063caef8a1f8adf08e71580b7fa971119cd335
PRINCE-CEEJAY-NG <your_email@example.com>
2025-08-26 22:45:47 +0100

    linked my social media and dumm contact logic in ContactSection component

--- src/pages/components/ContactSection.jsx
+++ src/pages/components/ContactSection.jsx
@@ -7,13 +7,56 @@ import {
   Twitter,
   Youtube,
   Send,
+  Instagram,
 } from "lucide-react";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 const ContactSection = () => {
+  const [contactForm, setContactForm] = useState({
+    username: "",
+    email: "",
+    message: "",
+    status: "",
+    loading: false,
+  });
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setContactForm((prev) => ({ ...prev, [name]: value }));
+  };
   const handleSubmit = (e) => {
+    setContactForm((prev) => ({
+      ...prev,
+      loading: true,
+      status: "Sending Messsage",
+    }));
     e.preventDefault();
+    setTimeout(() => {
+      setContactForm({
+        username: "",
+        email: "",
+        message: "",
+        loading: false,
+        status: "Message successfully sent to  Ceejay",
+      });
+      saveToLocalStorage();
+    }, 2000);
+  };
+  const saveToLocalStorage = () => {
+    const userData = {
+      name: contactForm.username,
+      email: contactForm.email,
+      message: contactForm.message,
+    };
+    try {
+      localStorage.setItem(
+        JSON.stringify(userData.email),
+        JSON.stringify(userData)
+      );
+    } catch (error) {
+      console.log("An error occured saving your details ", error);
+    }
   };
   return (
     <section id="contact" className="py-24 relative bg-secondary/30">
@@ -75,18 +118,46 @@ const ContactSection = () => {
             <div className="pt-8 mx-auto">
               <h4>Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="#" target="_blank">
+                <a
+                  href="https://web.facebook.com/princeceejay.ng.1"
+                  target="_blank"
+                  className="flex flex-col items-center"
+                >
+                  <p>Facebok</p>
                   <Facebook />
                 </a>
-                <a href="#" target="_blank">
+                <a
+                  href="https://www.youtube.com/@Prince_Ceejay_NG"
+                  target="_blank"
+                  className="flex flex-col items-center"
+                >
+                  <p>Youtube</p>
                   <Youtube />
                 </a>
-                <a href="#" target="_blank">
-                  <Twitter />
-                </a>
-                <a href="#" target="_blank">
+                <a
+                  href="https://www.linkedin.com/in/prince-ceejay-ng-01b544233/"
+                  target="_blank"
+                  className="flex flex-col items-center"
+                >
+                  <p>LinkedIn</p>
                   <Linkedin />
                 </a>
+                {/* <a
+                  href="#"
+                  target="_blank"
+                  className="flex flex-col items-center"
+                >
+                  <p>Twitter</p>
+                  <Twitter />
+                </a>
+                <a
+                  href="#"
+                  target="_blank"
+                  className="flex flex-col items-center"
+                >
+                  <p>Instagram</p>
+                  <Instagram />
+                </a> */}
               </div>
             </div>
           </div>
@@ -104,7 +175,9 @@ const ContactSection = () => {
                 <input
                   type="text"
                   id="name"
-                  name="name"
+                  name="username"
+                  value={contactForm.username}
+                  onChange={handleChange}
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
                   placeholder="John Wisky"
@@ -121,9 +194,11 @@ const ContactSection = () => {
                   type="email"
                   id="email"
                   name="email"
+                  value={contactForm.email}
+                  onChange={handleChange}
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary"
-                  placeholder="johnwisky@gmail.com"
+                  placeholder="example@gmail.com"
                 />
               </div>
               <div>
@@ -134,8 +209,11 @@ const ContactSection = () => {
                   Your Message
                 </label>
                 <textarea
+                  type="message"
                   id="message"
                   name="message"
+                  value={contactForm.message}
+                  onChange={handleChange}
                   required
                   className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary resize-none"
                   placeholder="Hello, I'd like to talk about ..."
@@ -143,6 +221,7 @@ const ContactSection = () => {
               </div>
               <button
                 type="submit"
+                disabled={contactForm.loading}
                 className={cn(
                   "cosmic-button cursor-pointer w-full flex items-center justify-center gap-2"
                 )}
@@ -150,6 +229,7 @@ const ContactSection = () => {
                 Send a Message
                 <Send size={16} />
               </button>
+              <h4 className="text-center text-primary">{contactForm.status}</h4>
             </form>
           </div>
         </div>
