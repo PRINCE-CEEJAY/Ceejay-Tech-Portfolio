c13568acc6afbc9c059b3f7a0eb84c7ac753b569
PRINCE CEEJAY <ceejaydroidprince@gmail.com>
2026-07-24 12:14:05 +0100

    modified project section to include first two deployed apps

--- src/pages/components/ProjectsSection.jsx
+++ src/pages/components/ProjectsSection.jsx
@@ -1,129 +1,130 @@
-import { ArrowRight, ExternalLink, Github } from "lucide-react";
-import React from "react";
+import { ArrowRight, ExternalLink, Github } from 'lucide-react';
+import React from 'react';
 const projects = [
   {
     id: 1,
-    title: "Landing Page",
-    description: "A landing page made with React and TailwindCSS",
-    image: "/projects/project1.jpg",
-    tags: ["React", "TailwindCss"],
-    demoURL: "#",
-    gitHubURL: "#",
+    title: 'Add to Cart System',
+    description: 'A shopping add to cart system',
+    image: '/projects/shop-add-to-cart.jpg',
+    tags: ['vite', 'context api', 'react query'],
+    demoURL: 'https://ceejay-add-to-cart-system.vercel.app',
+    gitHubURL: '#',
   },
   {
     id: 2,
-    title: "Multi-Function App",
-    description: "It consists of many functional components of react",
-    image: "/projects/project2.jpg",
-    tags: ["React", "Material", "TailwindCss"],
-    demoURL: "#",
-    gitHubURL: "#",
+    title: 'Advanced Fullstack Taskify CRUD',
+    description:
+      'an advanced fullstack crud app with filtering feature and framer-motion animations, connected with mongodb database',
+    image: '/projects/fullstack-crud.png',
+    tags: ['React', 'mongodb', 'TailwindCss', 'CRUD', 'todo', 'framer-motion'],
+    demoURL: 'https://ceejay-fullstack-crud.vercel.app',
+    gitHubURL: '#',
   },
   {
     id: 3,
-    title: "Note Taker",
+    title: 'Note Taker',
     description:
-      "Create and save your persistent notes to database, made with nexjs and mongoose",
-    image: "/projects/note-app.png",
-    tags: ["React", "NextJS", "Mongoose", "MongoDB", "TailwindCss"],
-    demoURL: "#",
-    gitHubURL: "#",
+      'Create and save your persistent notes to database, made with nexjs and mongoose',
+    image: '/projects/note-app.png',
+    tags: ['React', 'NextJS', 'Mongoose', 'MongoDB', 'TailwindCss'],
+    demoURL: '#',
+    gitHubURL: '#',
   },
   {
     id: 4,
-    title: "UNIVERSITY OF NIGERA NSUKKA - UNN CLONE!",
+    title: 'UNIVERSITY OF NIGERA NSUKKA - UNN CLONE!',
     description:
-      "coded to demostrate TailwindCSS and react skills, focus on UI",
-    image: "/projects/unn-clone.png",
-    tags: ["React", "TailwindCss"],
-    demoURL: "#",
-    gitHubURL: "#",
+      'coded to demostrate TailwindCSS and react skills, focus on UI',
+    image: '/projects/unn-clone.png',
+    tags: ['React', 'TailwindCss'],
+    demoURL: '#',
+    gitHubURL: '#',
   },
   {
     id: 5,
-    title: "NETFLIX CLONE!",
-    description: "Also made to demostrate TailwindCSS skills, focus on UI",
-    image: "/projects/netflix-clone.png",
-    tags: ["React", "TailwindCss"],
-    demoURL: "#",
-    gitHubURL: "#",
+    title: 'NETFLIX CLONE!',
+    description: 'Also made to demostrate TailwindCSS skills, focus on UI',
+    image: '/projects/netflix-clone.png',
+    tags: ['React', 'TailwindCss'],
+    demoURL: '#',
+    gitHubURL: '#',
   },
   {
     id: 6,
-    title: "YOUTUBE CLONE!",
-    description: "Created to demostrate TailwindCSS skills, focus on UI",
-    image: "/projects/youtube-clone.png",
-    tags: ["React", "TailwindCss"],
-    demoURL: "#",
-    gitHubURL: "#",
+    title: 'YOUTUBE CLONE!',
+    description: 'Created to demostrate TailwindCSS skills, focus on UI',
+    image: '/projects/youtube-clone.png',
+    tags: ['React', 'TailwindCss'],
+    demoURL: '#',
+    gitHubURL: '#',
   },
   {
     id: 7,
-    title: "MY PORTFOLIO version 1",
+    title: 'MY PORTFOLIO version 1',
     description:
-      "Created to demostrate TailwindCSS and react skills, with vite",
-    image: "/projects/myportfolio.png",
-    tags: ["React", "TailwindCss", "React", "Vite"],
-    demoURL: "#",
-    gitHubURL: "#",
+      'Created to demostrate TailwindCSS and react skills, with vite',
+    image: '/projects/myportfolio.png',
+    tags: ['React', 'TailwindCss', 'React', 'Vite'],
+    demoURL: '#',
+    gitHubURL: '#',
   },
 ];
 const ProjectsSection = () => {
   return (
-    <section id="projects" className="py-24 px-4 relative">
-      <div className="container mx-auto max-w-5xl">
-        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
-          Featured <span className="text-primary">Projects</span>
+    <section
+      id='projects'
+      className='py-24 px-4 relative'
+    >
+      <div className='container mx-auto max-w-5xl'>
+        <h2 className='text-3xl md:text-4xl font-bold mb-4 text-center'>
+          Featured <span className='text-primary'>Projects</span>
         </h2>
-        <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+        <p className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'>
           Here are some of my recent projects. Each project was carefully
           crafted with attention to detail, performance and user experience.
         </p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
           {projects.map((project, key) => (
             <div
               key={key}
-              className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+              className='group bg-card rounded-lg overflow-hidden shadow-xs card-hover'
             >
-              <div className="h-48 overflow-hidden">
-                <h1 className="text-center text-xl text-bold text-primary mb-1">
-                  {project.id}
-                </h1>
+              <div className='h-48 overflow-hidden'>
                 <img
                   src={project.image}
                   alt={project.title / project.id}
-                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                  className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-110'
                 />
               </div>
-              <div className="p-6">
-                <div className="flex flex-wrap justify-center gap-2 mb-4">
+              <div className='p-6'>
+                <div className='flex flex-wrap justify-center gap-2 mb-4'>
                   {project.tags.map((tag, key) => (
                     <span
                       key={key}
-                      className="px-2 py-1 font-medium border rounded-full bg-secondary bg-primary/20 text-secondary-foreground"
+                      className='px-2 py-1 font-medium border rounded-full bg-secondary bg-primary/20 text-secondary-foreground'
                     >
                       {tag}
                     </span>
                   ))}
                 </div>
-                <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
-                <p className="text-muted-foreground text-sm mb-4">
+                <h3 className='text-xl font-semibold mb-1'>{project.title}</h3>
+                <p className='text-muted-foreground text-sm mb-4'>
                   {project.description}
                 </p>
-                <div className="flex justify-center text-center">
-                  <div className="flex space-x-3 ">
+                <div className='flex justify-center text-center'>
+                  <div className='flex space-x-3 '>
                     <a
                       href={project.demoURL}
-                      target="_blank"
-                      className="text-foreground/80 hover:text-primary tranistion-colors duration-300"
+                      target='_blank'
+                      className='text-foreground/80 hover:text-primary tranistion-colors duration-300'
                     >
                       <ExternalLink size={20} />
                     </a>
                     <a
                       href={project.gitHubURL}
-                      target="_blank"
-                      className="text-foreground/80 hover:text-primary tranistion-colors duration-300"
+                      target='_blank'
+                      className='text-foreground/80 hover:text-primary tranistion-colors duration-300'
                     >
                       <Github size={20} />
                     </a>
