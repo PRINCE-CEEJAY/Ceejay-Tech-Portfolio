ec268c93e834db6d6b805fdd767241b7fb01da2c
PRINCE CEEJAY NG <92728956+PRINCE-CEEJAY@users.noreply.github.com>
2026-08-21 10:19:13 +0000

    projects hidden, refactoring it

--- src/pages/components/ProjectsSection.jsx
+++ src/pages/components/ProjectsSection.jsx
@@ -78,75 +78,82 @@ const projects = [
 ];
 const ProjectsSection = () => {
   return (
-    <section
-      id='projects'
-      className='py-24 px-4 relative'
-    >
-      <div className='container mx-auto max-w-5xl'>
-        <h2 className='text-3xl md:text-4xl font-bold mb-4 text-center'>
-          Featured <span className='text-primary'>Projects</span>
-        </h2>
-        <p className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'>
-          Here are some of my recent projects. Each project was carefully
-          crafted with attention to detail, performance and user experience.
-        </p>
+    // <section
+    //   id='projects'
+    //   className='py-24 px-4 relative'
+    // >
+    //   <div className='container mx-auto max-w-5xl'>
+    //     <h2 className='text-3xl md:text-4xl font-bold mb-4 text-center'>
+    //       Featured <span className='text-primary'>Projects</span>
+    //     </h2>
+    //     <p className='text-center text-muted-foreground mb-12 max-w-2xl mx-auto'>
+    //       Here are some of my recent projects. Each project was carefully
+    //       crafted with attention to detail, performance and user experience.
+    //     </p>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-          {projects.map((project, key) => (
-            <div
-              key={key}
-              className='group bg-card rounded-lg overflow-hidden shadow-xs card-hover'
-            >
-              <div className='overflow-hidden'>
-                <img
-                  src={project.image}
-                  alt={project.title / project.id}
-                  className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-110'
-                />
-              </div>
-              <div className='p-6'>
-                <div className='flex flex-wrap justify-center gap-2 mb-4'>
-                  {project.tags.map((tag, key) => (
-                    <span
-                      key={key}
-                      className='px-2 py-1 font-medium border rounded-full bg-secondary bg-primary/20 text-secondary-foreground'
-                    >
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-                <h3 className='text-xl font-semibold mb-1'>{project.title}</h3>
-                <p className='text-muted-foreground text-sm mb-4'>
-                  {project.description}
-                </p>
-                {project.deployed ? (
-                  <div className='flex justify-center text-center'>
-                    <div className='flex space-x-3 '>
-                      <a
-                        href={project.demoURL}
-                        target='_blank'
-                        className='text-foreground/80 hover:text-primary tranistion-colors duration-300'
-                      >
-                        <ExternalLink size={20} />
-                      </a>
-                      <a
-                        href={project.gitHubURL}
-                        target='_blank'
-                        className='text-foreground/80 hover:text-primary tranistion-colors duration-300'
-                      >
-                        <Github size={20} />
-                      </a>
-                    </div>
-                  </div>
-                ) : (
-                  <p className='italic text-center'>In Development...</p>
-                )}
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
+    //     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+    //       {projects.map((project, key) => (
+    //         <div
+    //           key={key}
+    //           className='group bg-card rounded-lg overflow-hidden shadow-xs card-hover'
+    //         >
+    //           <div className='overflow-hidden'>
+    //             <img
+    //               src={project.image}
+    //               alt={project.title / project.id}
+    //               className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-110'
+    //             />
+    //           </div>
+    //           <div className='p-6'>
+    //             <div className='flex flex-wrap justify-center gap-2 mb-4'>
+    //               {project.tags.map((tag, key) => (
+    //                 <span
+    //                   key={key}
+    //                   className='px-2 py-1 font-medium border rounded-full bg-secondary bg-primary/20 text-secondary-foreground'
+    //                 >
+    //                   {tag}
+    //                 </span>
+    //               ))}
+    //             </div>
+    //             <h3 className='text-xl font-semibold mb-1'>{project.title}</h3>
+    //             <p className='text-muted-foreground text-sm mb-4'>
+    //               {project.description}
+    //             </p>
+    //             {project.deployed ? (
+    //               <div className='flex justify-center text-center'>
+    //                 <div className='flex space-x-3 '>
+    //                   <a
+    //                     href={project.demoURL}
+    //                     target='_blank'
+    //                     className='text-foreground/80 hover:text-primary tranistion-colors duration-300'
+    //                   >
+    //                     <ExternalLink size={20} />
+    //                   </a>
+    //                   <a
+    //                     href={project.gitHubURL}
+    //                     target='_blank'
+    //                     className='text-foreground/80 hover:text-primary tranistion-colors duration-300'
+    //                   >
+    //                     <Github size={20} />
+    //                   </a>
+    //                 </div>
+    //               </div>
+    //             ) : (
+    //               <p className='italic text-center'>In Development...</p>
+    //             )}
+    //           </div>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </div>
+
+    // </section>
+
+    <div id='projects'>
+      <h1 className='text-5xl font-extrabold text-transparent bg-clip-text bg-linear-to-br from-green-900 via-blue-500 to-purple-800 text-center uppercase'>
+        Coming Soon !
+      </h1>
+    </div>
   );
 };
 
